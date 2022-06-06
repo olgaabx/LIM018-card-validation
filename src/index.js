55f1e93c62cedf444f9b3a8675f2daced60b0ff5
Olga Acosta <olgaacostab@gmail.com>
2022-06-06 11:08:24 -0500

    Funciona el isValid y el Maskify perfectamente. Test 9/9

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import validator from './validator.js';
-
 // Llamamos el input donde se va a ingresar el número de la tarjeta y poder usarlo en el evento de click
 //const creditCardNumber = document.getElementById('cardNumber');
-//console.log(creditCardNumber);
 const creditCardNumber = document.getElementById('cardNumber');
 // Llamamos el botón que envía los datos del formulario
 const payBtn = document.getElementById('buyTickets');
-//console.log(payBtn);
 
 // Iniciamos un evento click con un arrow function que va a tener una condición
 payBtn.addEventListener('click', (evt) => {
     // Usamos el preventDefault para poder ver los datos que ingresan en el input y no se refresque la página como lo hace el form por default
     evt.preventDefault();
     //const creditCardNumber = document.getElementById('cardNumber').value;
-    //console.log('El botón funciona');
-    //console.log(creditCardNumber.value);
+    //validator.isValid(creditCardNumber.value);
     let message = validator.isValid(creditCardNumber.value);
     //console.log(validator.isValid(creditCardNumber.value));
-    //validator.Maskify(creditCardNumber.value);
-    //let number = validator.Maskify(creditCardNumber.value);
-    //alert(number);
+    validator.maskify(creditCardNumber.value);
+    let number = validator.maskify(creditCardNumber.value);
+    alert(number);
 
     //console.log('Llamada antes del if')
-    /*if (message === true) {
-        alert("Esto está pasando");
+    if (message === true) {
+        alert("Tu tarjeta es correcta");
     } else {
-        message = "Tu tarjeta es inválida"
-    }*/
+        alert("Tu tarjeta es inválida");
+    }
 
     /*document.getElementById('resultMessage');
     //resultMessage.textContent = message;*/
--- src/validator.js
+++ src/validator.js
@@ -1,25 +1,47 @@
 const validator = {
  isValid:function(creditCardNumber){
-   console.log(creditCardNumber)
-   //let longitud = creditCardNumber.length;
-   //console.log('La longitud es tanto ' + longitud);
-   let ccNumber = creditCardNumber.split('').reverse();
-   console.log(ccNumber);
-   let reversed = ccNumber.join('');
+   //console.log(creditCardNumber)
+   const ccNumber = creditCardNumber.split('').reverse();
+   //let stringsNose;
+   let multiplyNumber = 0;
+   let multipliedNumbersArray = []
 
-    /*for(let i = 0; i<length; i++){
-      const currentDigit = creditCardNumber[i];
-      console.log(currentDigit);
-    }*/
+   for (let i = 0; i < ccNumber.length; i++){
+     let arrInd = ccNumber[i];
+     if (i === 0) {
+      multiplyNumber = arrInd*1;
+     }
+      else if (i%2 === 1){
+        multiplyNumber = arrInd*2;
+     }
+      else {
+        multiplyNumber = arrInd*1;
+      }
+     if (multiplyNumber > 9) {
+       multiplyNumber -= 9
+     }
+    // console.log(multiplyNumber);    
+      multipliedNumbersArray.push(multiplyNumber);
+      //console.log(multipliedNumbersArray);
+   }
+
+   let result = 0;
+   for (let i = 0; i < multipliedNumbersArray.length; i++) {
+    result = result + multipliedNumbersArray[i];
+    //console.log(result);
+   }
+
+   return result % 10 === 0
   },
 
-  /*Maskify:function(creditCardNumber){
+  maskify:function(creditCardNumber){
     let arrayNew = [];
     let numCard;
     let array = creditCardNumber.split('');
 
     // usamos un forEach para que ocurra 
     array.forEach((element, i) => {
+      //console.log(element);
       if (i < array.length - 4 ){
         element = "#";
       }
@@ -30,11 +52,8 @@ const validator = {
     });
 
     numCard = arrayNew.join('');
-    //alert(numCard);
     return numCard;
-  }*/
+  }
 }
-
-
 
 export default validator;
